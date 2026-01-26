522fe524c141376763f6e262c40efb3582503b2e
G-mohamed3 <gm97530335@gmail.com>
2026-01-26 19:39:24 +0100

    Suppression

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,6 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: 'EduManager - Plateforme de gestion pédagogique',
   description: 'Gérez vos formations avec simplicité et efficacité.',
-  icons: {
-    ,
   },
 }
 
